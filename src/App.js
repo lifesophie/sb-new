0f2a76820890ddec035cee2d1ceb88ec6fa537c6
lifesophie <fsmfsm777@gmail.com>
2024-12-31 16:48:37 +0300

    Update my lovely project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,34 @@
 import logo from './logo.svg';
-import './App.css';
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import HomePage from "./components/Homepage";
+import About from "./components/About";
+import Contacts from "./components/Contacts";
+import CourseList from "./components/CourseList";
+import Services from './components/Services';
+import Course from './components/Course';
+import OneCourse from './components/OneCourse';
+
+// src/App.js
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <Router>
+            <Header />
+            <Routes>
+                <Route path="/" element={<HomePage />} />
+                <Route path="/course" element={<Course />} />
+                <Route path="/services" element={<Services />} />
+                <Route path="/about" element={<About />} />
+                <Route path="/contacts" element={<Contacts />} />
+                <Route path="/courselist" element={<CourseList />} />
+                <Route path="/onecourse/:id" element={<OneCourse />} />
+            </Routes>
+            <Footer />
+        </Router>
+    );
 }
 
 export default App;
--- src/reportWebVitals.js
+++ src/reportWebVitals.js
@@ -1,13 +1,14 @@
-const reportWebVitals = onPerfEntry => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
-};
+// src/reportWebVitals.js
+import { onCLS, onFID, onFCP, onLCP, onTTFB } from 'web-vitals';
+
+function reportWebVitals(onPerfEntry) {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        onCLS(onPerfEntry);
+        onFID(onPerfEntry);
+        onFCP(onPerfEntry);
+        onLCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+    }
+}
 
 export default reportWebVitals;
