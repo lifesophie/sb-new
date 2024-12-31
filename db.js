7fb1fe576262c397d62b0ccb583559433e64bda5
lifesophie <fsmfsm777@gmail.com>
2024-12-31 20:01:02 +0300

    Update server code vercal

--- db.js
+++ db.js
@@ -1,12 +1,11 @@
-// db.js
 const { Pool } = require('pg');
 
 const pool = new Pool({
-    user: 'postgres', // Имя пользователя базы данных
-    host: 'localhost',
-    database: 'postgres', // Имя базы данных
-    password: 'S74786719aPROFI2024', // Пароль пользователя базы данных
-    port: 5433, // Порт базы данных (по умолчанию 5432)
+    user: process.env.DB_USER, // Имя пользователя базы данных
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME, // Имя базы данных
+    password: process.env.DB_PASSWORD, // Пароль пользователя базы данных
+    port: process.env.DB_PORT, // Порт базы данных (по умолчанию 5432)
 });
 
 module.exports = pool;
